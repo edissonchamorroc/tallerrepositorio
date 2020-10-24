8c90016b231b924f93c4d62241374ce9f4b16143
edisson chamorro <john.chamorro@udea.edu.co>
2020-10-23 19:22:43 -0500

    ya me funciona la lectura, problema con datos en fichero

--- main.cpp
+++ main.cpp
@@ -34,159 +34,161 @@ int main()
     escribir.close();*/
 
 //*****************************************************
-   int clave,Nclave, opcion, comparadorclave;
-   char nombre[30],Nnombre[30];
-   ofstream guardar;
-   ofstream temp;//para escritura temporal
-   ifstream leer;
-   guardar.open("fichero.txt",ios::app);//app se para en la ultima linea si dañar datos ingresados
-   //con ate se para al final mientras el archivo está abierto, si se cierra se dañan los datos
-   while(true){
-       cout<<"1. Guardar Registro"<<endl;
-       cout<<"2. Leer Registro"<<endl;
-       cout<<"3. clave a buscar"<<endl;
-       cout<<"4. clave a eliminar"<<endl;
-       cout<<"5. moficar clave"<<endl;
-       cout<<"Ingreso opcion: ";
-       cin>>opcion;
-       switch (opcion) {
-         case 1:{
-              cout<<"Ingrese nombre: "<<endl;
-              cin>>nombre;
-              cout<<"Ingrese clave: "<<endl;
-              cin>>clave;//cin solo lee hasta donde haya espacio
-              guardar<<nombre<<" "<<clave<<endl;
-               break;}
-         case 2:{
-                   leer.open("fichero.txt");
-                   if(leer.is_open())
-                   {
-                   leer>>nombre;//>> con este solo lee hasta el primer espacio
-                   while(!leer.eof())
-                   {
-                       leer >> clave;//ojo con el dato de clave, se debe poder representar en 4 bytes
-                       cout << "Nombre: " << nombre << endl;
-                       cout << "Clave: "<< clave<<endl;
-                       cout<<endl;
-                       leer >> nombre;
+    int clave,Nclave, opcion, comparadorclave;
+    char nombre[30];
+    ofstream guardar;
+    ofstream temp;//para escritura temporal
+    ifstream leer;
 
-                   }
+    //con ate se para al final mientras el archivo está abierto, si se cierra se dañan los datos
+    while(true){
+        cout<<"1. Guardar Registro"<<endl;
+        cout<<"2. Leer Registro"<<endl;
+        cout<<"3. clave a buscar"<<endl;
+        cout<<"4. clave a eliminar"<<endl;
+        cout<<"5. moficar clave"<<endl;
+        cout<<"Ingreso opcion: ";
+        cin>>opcion;
+        switch (opcion) {
+        case 1:{
+            guardar.open("fichero.txt",ios::app);//app se para en la ultima linea si dañar datos ingresados
+            cout<<"Ingrese nombre: "<<endl;
+            cin>>nombre;
+            cout<<"Ingrese clave: "<<endl;
+            cin>>clave;//cin solo lee hasta donde haya espacio
+            guardar<<nombre<<" "<<clave<<endl;
+            guardar.close();
+            break;}
+        case 2:{
+            leer.open("fichero.txt");
+            if(leer.is_open())
+            {
+                leer>>nombre;//>> con este solo lee hasta el primer espacio
+                while(!leer.eof())
+                {
+                    leer >> clave;//ojo con el dato de clave, se debe poder representar en 4 bytes
+                    cout << "Nombre: " << nombre << endl;
+                    cout << "Clave: "<< clave<<endl;
+                    cout<<endl;
+                    leer >> nombre;
 
-                   leer.close();
-                  }
-                  else
-                   {
-                       cout<<"El archivo no existe"<<endl;
-                   }
-                   break;
-               }
-       case 3:{
-           leer.open("fichero.txt");
-           leer>>nombre;
-           bool encontrado=false;
-           cout<<"Ingrese clave a buscar:";
-           cin>>comparadorclave;
-           while(!leer.eof())
-           {
-             leer>>clave;//
-             if(clave==comparadorclave)
-             {
-                 cout<<"Nombre: " <<nombre<<endl;
-                 cout<<"Clave: " <<clave<<endl;
-                 encontrado=true;
-             }
-             leer>>nombre;
-           }
-           if(encontrado==false)
-           {
-               cout<<"\nContrasena no existe "<<endl;
-           }
-           leer.close();
-           break;
+                }
 
-       }
-       case 4:
-       {
-           leer.open("fichero.txt");
-           temp.open("temp.txt");//creacion de un nuevo archivo
-           leer>>nombre;
-           bool encontrado=false;
-           cout<<"Ingrese clave a eliminar:";
-           cin>>comparadorclave;
-           while(!leer.eof())
-           {
-             leer>>clave;//
-             if(clave==comparadorclave)
-             {
-                 cout<<"Nombre: " <<nombre<<endl;
-                 cout<<"Clave: " <<clave<<endl;
-                 encontrado=true;
-                 cout<<"Eliminado"<<endl;
-             }
-             else
-             {
-                 temp<<nombre<<clave<<endl;
-             }
-             leer>>nombre;
-           }
-           if(!encontrado)
-           {
-               cout<<"\nEl registro no existe "<<endl;
-           }
-           leer.close();
-           temp.close();
-           guardar.close();
-           remove("fichero.txt");
-           rename("temp.txt","fichero.txt");
-           guardar.open("fichero.txt",ios::app);
-           break;
+                leer.close();
+            }
+            else
+            {
+                cout<<"El archivo no existe"<<endl;
+            }
+            break;
+        }
+        case 3:{
+            leer.open("fichero.txt");
+            leer>>nombre;
+            bool encontrado=false;
+            cout<<"Ingrese clave a buscar:";
+            cin>>comparadorclave;
+            while(!leer.eof())
+            {
+                leer>>clave;//
+                if(clave==comparadorclave)
+                {
+                    cout<<"Nombre: " <<nombre<<endl;
+                    cout<<"Clave: " <<clave<<endl;
+                    encontrado=true;
+                }
+                leer>>nombre;
+            }
+            if(encontrado==false)
+            {
+                cout<<"\nContrasena no existe "<<endl;
+            }
+            leer.close();
+            break;
 
-       }
-       case 5:
-       {
-           leer.open("fichero.txt");
-           temp.open("temp.txt");//creacion de un nuevo archivo
-           leer>>nombre;
-           bool encontrado=false;
-           cout<<"Ingrese clave a modificar:";
-           cin>>comparadorclave;
-           while(!leer.eof())
-           {
-             leer>>clave;//
-             if(clave==comparadorclave)
-             {
-                 cout<<"Nombre: " <<nombre<<endl;
-                 cout<<"Clave: " <<clave<<endl;
-                 encontrado=true;
+        }
+        case 4:
+        {
+            leer.open("fichero.txt");
+            temp.open("temp.txt");//creacion de un nuevo archivo
+            leer>>nombre;
+            bool encontrado=false;
+            cout<<"Ingrese clave a eliminar:";
+            cin>>comparadorclave;
+            while(!leer.eof())
+            {
+                leer>>clave;//
+                if(clave==comparadorclave)
+                {
+                    cout<<"Nombre: " <<nombre<<endl;
+                    cout<<"Clave: " <<clave<<endl;
+                    encontrado=true;
+                    cout<<"Eliminado"<<endl;
+                }
+                else
+                {
+                    temp<<nombre<<" "<<clave<<endl;
+                }
+                leer>>nombre;
+            }
+            if(!encontrado)
+            {
+                cout<<"\nEl registro no existe "<<endl;
+            }
+            leer.close();
+            temp.close();
+            guardar.close();
+            remove("fichero.txt");
+            rename("temp.txt","fichero.txt");
+            guardar.open("fichero.txt",ios::app);
+            break;
 
-                 cout<<"Ingrese nueva Clave"<<endl;
-                 cin>>Nclave;
-                 temp<<nombre<<" "<<Nclave<<endl;
-             }
-             else
-             {
-                 temp<<nombre<<clave<<endl;
-             }
-             leer>>nombre;
-           }
-           if(!encontrado)
-           {
-               cout<<"\nEl registro no existe "<<endl;
-           }
-           leer.close();
-           temp.close();
-           guardar.close();
-           remove("fichero.txt");
-           rename("temp.txt","fichero.txt");
-           guardar.open("fichero.txt",ios::app);
-           _sleep(3500);
-           system("CLS");
-           break;
+        }
+        case 5:
+        {
+            leer.open("fichero.txt");
+            temp.open("temp.txt");//creacion de un nuevo archivo
+            leer>>nombre;
+            bool encontrado=false;
+            cout<<"Ingrese clave a modificar:";
+            cin>>comparadorclave;
+            while(!leer.eof())
+            {
+                leer>>clave;//
+                if(clave==comparadorclave)
+                {
+                    cout<<"Nombre: " <<nombre<<endl;
+                    cout<<"Clave: " <<clave<<endl;
+                    encontrado=true;
 
-       }
-     }
-   }
-//*****************************************************
+                    cout<<"Ingrese nueva Clave"<<endl;
+                    cin>>Nclave;
+                    temp<<nombre<<" "<<Nclave<<endl;
+                }
+                else
+                {
+                    temp<<nombre<<clave<<endl;
+                }
+                leer>>nombre;
+            }
+            if(!encontrado)
+            {
+                cout<<"\nEl registro no existe "<<endl;
+            }
+            leer.close();
+            temp.close();
+            guardar.close();
+            remove("fichero.txt");
+            rename("temp.txt","fichero.txt");
+            guardar.open("fichero.txt",ios::app);
+            system("pause");
+            system("CLS");
+            break;
+
+        }
+        }
+    }
+    //*****************************************************
 
 
     return 0;
